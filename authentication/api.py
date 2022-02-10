f9aab6e3cfc30c2f56124ab9a4a8cf32ec58ceeb
Rajat-coder <handarajat111@gmail.com>
2022-02-10 22:45:41 +0530

    changes in views

--- authentication/api.py
+++ authentication/api.py
@@ -6,14 +6,18 @@ from authentication.serializers import AssociatesMasterSerializer
 from authentication.models import AssociatesMasterModel
 
 class AssociateMasterAPIView(APIView):
+    #for getting data by id , by serach associate name and all data
     def get(self,request,*args,**kwargs):
         request_data = request.GET
         output_data, output_status = [], False
         res_status = status.HTTP_400_BAD_REQUEST
+        # if we are getting id 
         if request_data.get("id",False):
             ass_obj = AssociatesMasterModel.objects.filter(pk = id).first()
+        # if we are getting search parameter
         elif request_data.get("search",False):
             ass_obj = AssociatesMasterModel.objects.filter(name__icontains = request_data['search']).first()
+        # all data
         else:
             ass_obj = AssociatesMasterModel.objects.all()
         if ass_obj:
@@ -41,10 +45,12 @@ class AssociateMasterAPIView(APIView):
     def post(self,request,*args,**kwargs):
         output_data, output_status = [], False
         res_status = status.HTTP_400_BAD_REQUEST
+        # if we want to update by id 
         if request.GET.get("id",False):
             instance = AssociatesMasterModel.obejcts.filter(pk = id).first()
             if instance:
                 return self.update(request, instance)
+        # if new data we want to add
         serializer = AssociatesMasterSerializer(data = request.data,partial = True)
         if serializer.is_valid():
             serializer.save()
@@ -59,6 +65,7 @@ class AssociateMasterAPIView(APIView):
         return Response(context,status = res_status,content_type="application/json")
         
     def delete(self,request):
+        # for deleting obj
         output_data, output_status = [], False
         res_status = status.HTTP_400_BAD_REQUEST
         instance = AssociatesMasterModel.objects.filter(pk = request.GET.get("id")).first()
